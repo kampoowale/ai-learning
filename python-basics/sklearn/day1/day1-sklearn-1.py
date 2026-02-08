f6313292325842cd0b10f55ff686edee323d8e0c
ChitsNits <chitsnits@Mac.Home>
2026-02-08 15:23:02 +0000

    scaling

--- python-basics/sklearn/day1/day1-sklearn-1.py
+++ python-basics/sklearn/day1/day1-sklearn-1.py
@@ -24,7 +24,7 @@ print("X and y", X, y)
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42)
 
-model = LogisticRegression()
+model = LogisticRegression(max_iter=1000)
 
 model.fit(X_train, y_train)
 
--- python-basics/sklearn/day1/day1-sklearn.py
+++ python-basics/sklearn/day1/day1-sklearn.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
-
+from sklearn.preprocessing import StandardScaler
 # Load data
 df = pd.read_csv("data-students-score.csv")
 print("DF", df)
@@ -31,7 +31,6 @@ print("Calue of y : ", y)
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
-
 # What this does
 # X → your features (Age & Score)
 # y → your target (Passed True/False)
@@ -40,18 +39,30 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 # Model
 model = LogisticRegression()
+scaler = StandardScaler()
 
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
 # Train
-model.fit(X_train, y_train)
+model.fit(X_train_scaled, y_train)
 
 # Predict
-y_pred = model.predict(X_test)
+y_pred = model.predict(X_test_scaled)
 
-random = [[3, 6]]
-prediction = model.predict(random)
-print("Prediction random data", prediction)
+# random = [[3, 6]]
+# prediction = model.predict(random)
+# print("Prediction random data", prediction)
 
 
 print("Prediction : ", X_test, y_pred)
 # Evaluate
 print("Accuracy:", accuracy_score(y_test, y_pred))
+
+test_cases = [
+    [23, 56],  # low score false
+    [3, 75],  # pass false
+    [25, 90],  # strong pass true
+    [22, 69],  # borderline fail false
+]
+X_test_scaled = scaler.transform([[3, 75]])
+print(model.predict(test_cases))
